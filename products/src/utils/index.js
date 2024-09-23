8ce9c540d47c9b16d58caef8da459aa785822728
mk06005@gmail.com <mk065005@gmail.com>
2024-09-23 09:34:47 +0530

    replacing - web hook with message broker

--- products/src/utils/index.js
+++ products/src/utils/index.js
@@ -50,18 +50,3 @@ module.exports.FormateData = (data) => {
     throw new Error("Data Not found!");
   }
 };
-
-
-module.exports.PublishCustomerEvent = async(payload) => {
-  axios.post('http://localhost:8000/customer/app-events', {
-    payload
-  })
-  // perform some operation
-}
-
-module.exports.PublishShoppingEvent = async(payload) => {
-  axios.post('http://localhost:8000/shopping/app-events', {
-    payload
-  })
-  // perform some operation
-}
