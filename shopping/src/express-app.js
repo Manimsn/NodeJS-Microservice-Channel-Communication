a81825f7a045730553de329085c170866dd96896
mk06005@gmail.com <mk065005@gmail.com>
2024-10-04 23:16:46 +0530

    Adding

--- shopping/src/express-app.js
+++ shopping/src/express-app.js
@@ -6,7 +6,7 @@ const HandleErrors = require('./utils/error-handler')
 
 module.exports = async (app,channel) => {
 
-    app.use(express.json({ limit: '1mb'}));
+    app.use(express.json({ limit: '1mb'}))
     app.use(express.urlencoded({ extended: true, limit: '1mb'}));
     app.use(cors());
     app.use(express.static(__dirname + '/public'))
