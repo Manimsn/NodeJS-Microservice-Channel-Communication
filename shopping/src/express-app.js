2892624d1f2d7be4aaea4cd12b463111981c506c
mk06005@gmail.com <mk065005@gmail.com>
2024-10-05 22:34:06 +0530

    Adding

--- shopping/src/express-app.js
+++ shopping/src/express-app.js
@@ -8,7 +8,7 @@ module.exports = async (app,channel) => {
 
     app.use(express.json({ limit: '1mb'}));
     app.use(express.urlencoded({ extended: true, limit: '1mb'}));
-    app.use(cors());
+    app.use(cors())
     app.use(express.static(__dirname + '/public'));
 
     // appEvents(app);
